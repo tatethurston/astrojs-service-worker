3fef7bda11957d7de3a0a729d022df58d1d1bb82
Tate Thurston <tatethurston@gmail.com>
2023-08-30 14:13:14 -0700

    fix Astro v3 Response warning (#20)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.1.0
+
+- Adds support for [base](https://docs.astro.build/en/reference/configuration-reference/#base). Thanks @ntsd.
+- Fix Response warning for Astro v3 users: https://docs.astro.build/en/guides/upgrade-to/v3/#deprecated-returning-simple-object-in-endpoints.
+
 ## 1.0.0
 
 - astrojs-service-worker is now 1.0! This library will now follow [semantic versioning](https://docs.npmjs.com/about-semantic-versioning).
--- src/service-worker.js.js
+++ src/service-worker.js.js
@@ -1,7 +1,7 @@
 import { readFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 
-export async function get() {
+export async function GET() {
   const sw = await readFile(
     // eslint-disable-next-line no-undef
     fileURLToPath(new URL("./noop-service-worker.js", import.meta.url)),
@@ -9,7 +9,8 @@ export async function get() {
       encoding: "utf8",
     },
   );
-  return {
-    body: sw,
-  };
+
+  const headers = new Headers();
+  headers.append("content-type", "application/javascript;charset=utf-8");
+  return new Response(sw, { headers });
 }
